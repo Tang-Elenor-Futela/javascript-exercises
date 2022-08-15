5185b181e3a6f4b2092e747af38da5ffa4ebbf53
El-Eanor <eleanortang233@gmail.com>
2022-08-15 08:01:34 -0700

    dom 4, querry selector

---
+++ exercise.js
@@ -0,0 +1,88 @@
+
+//dom one
+
+// function js_style() 
+// {
+// //font styles added by JS:
+//  text.style.fontSize = "14pt";
+//  text.style.fontFamily = "Comic Sans MS";
+//  text.style.color = "green";
+// }
+
+
+//dom 2
+// let form = document.querySelector('#form1');
+// const welcome = document.querySelector('.welcome');
+
+// form.addEventListener('submit', e => {
+// e.preventDefault();
+// welcome.textContent = `Hello, ${form.fname.value} ${form.lname.value}!`;
+// });
+
+
+
+//dom 3 
+// BodyElements = document.querySelectorAll("para")
+
+// myp1 = BodyElements[0];
+// myp1.style.background = "rgb(255,0,0)";
+
+// myp2 = BodyElements[1];
+// myp2.style.background = "rgb(255,255,0)"
+   
+
+
+
+
+//Dom 4
+// function getAttributes() {
+//     var href = document.getElementById("w3r").href;
+//     var hreflang = document.getElementById("w3r").hreflang;
+//     var rel = document.getElementById("w3r").rel;
+//     var target = document.getElementById("w3r").target;
+//     var type = document.getElementById("w3r").type;
+//     console.log("href: " + href + "\nhreflang: " + hreflang + "\nrel: " + rel + "\ntarget: " + target + "\ntype: " + type);
+//     }
+
+
+
+
+
+//Dom 5
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let jsstyle= document.querySelector ('#jsstyle');
+// console.log(jsstyle)
+// jsstyle=(StyleSheet)
+
+// const addEventListener('click' )
+
+
+// function getAttributes()
+// {
+//  var u = document.getElementById("w3r").href;
+//  alert('The value of the href attribute of the link is : '+u);
+//  var v = document.getElementById("w3r").hreflang;   
+//  alert('The value of the hreflang attribute of the link is : '+v);
+//  var w = document.getElementById("w3r").rel; 
+//   alert('The value of the rel attribute of the link is : '+w);
+//  var x = document.getElementById("w3r").target; 
+//   alert('The value of the taget attribute of the link is : '+x);
+//  var y = document.getElementById("w3r").type; 
+//   alert('The value of the type attribute of the link is : '+y);  
+// }
